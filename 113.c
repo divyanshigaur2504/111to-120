dbb6468ef1b412691b8d89b6d8addf3954ac3d91
divyanshigaur2504 <divyanshigaur108@gmail.com>
2025-12-04 22:23:26 +0530

    Create 113.c

---
+++ 113.c
@@ -0,0 +1,59 @@
+/*Q113: Write a program to take an integer array arr and an integer k as inputs. The task is to find the kth smallest element in the array. Print the kth smallest element as output.
+
+/*
+Sample Test Cases:
+Input 1:
+arr[] = [10, 5, 4, 3, 48, 6, 2, 33, 53, 10], k = 4
+Output 1:
+5
+
+Input 2:
+arr[] = [7, 10, 4, 3, 20, 15], k = 3
+Output 2:
+7
+
+*/
+
+
+#include <stdio.h>
+
+// Function to sort the array (simple bubble sort for clarity)
+void sortArray(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, k;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    // Sort the array
+    sortArray(arr, n);
+
+    // kth smallest element is at index k-1
+    if (k > 0 && k <= n) {
+        printf("The %dth smallest element is %d\n", k, arr[k - 1]);
+    } else {
+        printf("Invalid k (must be between 1 and %d)\n", n);
+    }
+
+    return 0;
+}
